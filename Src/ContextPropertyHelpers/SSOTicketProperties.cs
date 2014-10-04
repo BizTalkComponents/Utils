fbc683e2ea23714d9f16ae08d6befbd531f30310
robinhultman <robin.hultman@gmail.com>
2014-10-04 15:56:05 +0200

    Added class with constant sso ticket property reference

---
+++ Src/ContextPropertyHelpers/SSOTicketProperties.cs
@@ -0,0 +1,7 @@
+﻿namespace BizTalkComponents.Utils.ContextPropertyHelpers
+{
+    public class SSOTicketProperties
+    {
+        public const string SSOTicket = "http://schemas.microsoft.com/BizTalk/2003/system-properties#SSOTicket"; 
+    }
+}
