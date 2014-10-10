b5b66be8e5314670992ff4e3ab1c0b8543c7c122
robinhultman <robin.hultman@gmail.com>
2014-10-10 07:30:07 +0200

    Moved CopyContext from helper class to the context extension class

--- Src/Utils/ContextPropertyHelpers/ContextExtensions.cs
+++ Src/Utils/ContextPropertyHelpers/ContextExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.BizTalk.Message.Interop;
+﻿using System;
+using Microsoft.BizTalk.Message.Interop;
 
 namespace BizTalkComponents.Utils.ContextPropertyHelpers
 {
@@ -17,6 +18,28 @@ namespace BizTalkComponents.Utils.ContextPropertyHelpers
         public static void Write(this IBaseMessageContext ctx, ContextProperty property, string val)
         {
             ctx.Write(property.PropertyName, property.PropertyNamespace,val);
+        }
+
+        public static void Copy(this IBaseMessageContext ctx, ContextProperty source, ContextProperty destination)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (destination == null)
+            {
+                throw new ArgumentNullException("destination");
+            }
+
+            string sourceValue;
+
+            if (ctx.TryRead(source, out sourceValue))
+            {
+                throw new InvalidOperationException("Could not find the specified source property in BizTalk context.");
+            }
+
+            ctx.Promote(destination, sourceValue);
         }
     }
 }
