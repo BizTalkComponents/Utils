7823c1e6f992fe18f02e34acc159bfa95c29468d
robinhultman <robin.hultman@gmail.com>
2014-10-17 11:25:31 +0200

    More system property constants Added MessageType and SchemaStrongName

---
+++ Src/Utils/ContextPropertyHelpers/SystemProperties.cs
@@ -0,0 +1,8 @@
+﻿namespace BizTalkComponents.Utils.ContextPropertyHelpers
+{
+    public class SystemProperties
+    {
+        public const string MessageType = "http://schemas.microsoft.com/BizTalk/2003/system-properties#MessageType";
+        public const string SchemaStrongName = "http://schemas.microsoft.com/BizTalk/2003/system-properties#SchemaStrongName";
+    }
+}
