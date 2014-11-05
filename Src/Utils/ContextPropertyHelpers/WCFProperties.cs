c2d7c3a3904b632798afe735a3af738e3039a192
robinhultman <robin.hultman@gmail.com>
2014-11-05 07:04:04 +0100

    Added context property helper for WCF properties
    Also added property for outbound http status

---
+++ Src/Utils/ContextPropertyHelpers/WCFProperties.cs
@@ -0,0 +1,8 @@
+﻿namespace BizTalkComponents.Utils.ContextPropertyHelpers
+{
+    public class WCFProperties
+    {
+        public const string OutboundHttpStatusCode =
+            "http://schemas.microsoft.com/BizTalk/2006/01/Adapters/WCF-properties#OutboundHttpStatusCode";
+    }
+}
