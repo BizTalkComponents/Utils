d6e4034efb9ff07129431a2cb966cb2dbfaed05f
robinhultman <robin.hultman@gmail.com>
2014-11-06 09:19:50 +0100

    Restructured project to end up with 1 NuGet package per utility

--- Src/Utils/ContextPropertyHelpers/SSOTicketProperties.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BizTalkComponents.Utils.ContextPropertyHelpers
-{
-    public class SSOTicketProperties
-    {
-        public const string SSOTicket = "http://schemas.microsoft.com/BizTalk/2003/system-properties#SSOTicket"; 
-    }
-}
--- Src/Utils/ContextPropertyHelpers/WCFProperties.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BizTalkComponents.Utils.ContextPropertyHelpers
-{
-    public class WCFProperties
-    {
-        public const string OutboundHttpStatusCode =
-            "http://schemas.microsoft.com/BizTalk/2006/01/Adapters/WCF-properties#OutboundHttpStatusCode";
-    }
-}
