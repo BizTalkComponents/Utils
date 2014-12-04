be5afa5d1fcb137455b8a0f9f7a031f000525901
robinhultman <robin.hultman@gmail.com>
2014-12-04 07:15:53 +0100

    Unit tests Added unit test project

---
+++ Tests/UnitTests/ContextExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Winterdom.BizTalk.PipelineTesting;
+
+namespace BizTalkComponents.Utils.Tests.UnitTests
+{
+    [TestClass]
+    public class ContextExtensionTests
+    {
+      
+    }
+}
