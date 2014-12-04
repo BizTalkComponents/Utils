9597adcda493c3c7ba13cd98788ca26fd480560a
robinhultman <robin.hultman@gmail.com>
2014-12-04 07:18:41 +0100

    Added basic setup for creating a new test message with a promoted property

--- Tests/UnitTests/ContextExtensionTests.cs
+++ Tests/UnitTests/ContextExtensionTests.cs
@@ -8,6 +8,13 @@ namespace BizTalkComponents.Utils.Tests.UnitTests
     [TestClass]
     public class ContextExtensionTests
     {
-      
+        private IBaseMessage _testMessage;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            _testMessage = MessageHelper.Create("<test></test>");
+            _testMessage.Context.Promote(new ContextProperty("http://testuri.org#SourceProperty"), "Value");
+        }
     }
 }
